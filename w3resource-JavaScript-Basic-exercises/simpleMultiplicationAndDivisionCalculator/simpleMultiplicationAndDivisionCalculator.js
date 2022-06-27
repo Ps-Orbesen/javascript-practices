0394386f3375ba27266eda0fd8d456b799e16e24
Patiphan Orbesen <porbesen@cnm.edu>
2022-06-27 11:24:31 -0600

    added simpleMultiplicationAndDivisionCalculator.js and index.html

---
+++ w3resource-JavaScript-Basic-exercises/simpleMultiplicationAndDivisionCalculator/simpleMultiplicationAndDivisionCalculator.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to calculate multiplication and division of two numbers (input from user).
+// =======================================================================================================
+
+
+// Create a function for multiplication
+
+function multiplication()
+{
+    input1 = document.getElementById("num1").value;
+    input2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = input1 * input2;
+}
