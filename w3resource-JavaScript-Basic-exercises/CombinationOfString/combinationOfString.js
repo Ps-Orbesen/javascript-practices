5094a23d02bb003fb74ab484d0fd327a7e2ae96b
Patiphan Orbesen <porbesen@cnm.edu>
2022-07-08 11:59:35 -0600

    finished function combinationOfString

--- w3resource-JavaScript-Basic-exercises/CombinationOfString/combinationOfString.js
+++ w3resource-JavaScript-Basic-exercises/CombinationOfString/combinationOfString.js
@@ -5,11 +5,28 @@ function combinationOfString(str1) {
     // Create empty array
     let array1 = [];
         for (let x = 0, y = 1; x < str1.length; x=x+1,y=y+1) {
-            array1[x] = str1.combinationOfString(x,y);
+            array1[x] = str1.substring(x,y);
         }
     let combination = [];
     let temp = "";
     let slent = Math.pow(2, array1.length);
 
+        for (let i = 0; i < slent; i=i+1) {
+            temp = " ";
+            for (let j = 0; j < array1.length; j = j+1) {
+                if ((i & Math.pow(2,j))) {
+                    temp += array1[j];
+
+                }
+            }
+            if (temp !== " ") {
+                combination.push(temp);
+            }
+
+        }
+
+        console.log(combination.join("\n"));
 
 }
+
+combinationOfString("fond");
