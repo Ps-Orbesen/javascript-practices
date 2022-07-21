eb293355650f0527796a16b0fd05a49433d22d36
Patiphan Orbesen <porbesen@cnm.edu>
2022-07-21 11:17:26 -0600

    added script.js

---
+++ freeCodeCamp-JavaScript-Projects/number-counter/script.js
@@ -0,0 +1,11 @@
+
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(function (item) {
+    console.log(item);
+});
