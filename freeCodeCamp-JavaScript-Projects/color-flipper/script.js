b017b9588212d2470918f3ebddeb7aec70462cf4
Patiphan Orbesen <porbesen@cnm.edu>
2022-09-28 22:07:17 -0600

    setup buttons event listener

--- freeCodeCamp-JavaScript-Projects/color-flipper/script.js
+++ freeCodeCamp-JavaScript-Projects/color-flipper/script.js
@@ -4,3 +4,12 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]
 const btn = document.getElementById('btn');
 // Color span
 const color = document.querySelector(".color");
+
+// Add buttons event listener
+btn.addEventListener('click', function (){
+    // get random number between 0-3
+    const randomNum = 2;
+    document.body.style.backgroundColor = colors[randomNum];
+    color.textContent = colors[randomNum];
+
+});
