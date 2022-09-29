49888539244d1fee6c6ce68b4c9d8fd93c32c072
Patiphan Orbesen <porbesen@cnm.edu>
2022-09-28 22:02:14 -0600

    setup targeting buttons in script.js

---
+++ freeCodeCamp-JavaScript-Projects/color-flipper/script.js
@@ -0,0 +1,6 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]
+
+// Targeting buttons
+const btn = document.getElementById('btn');
+// Color span
+const color = document.querySelector(".color");
