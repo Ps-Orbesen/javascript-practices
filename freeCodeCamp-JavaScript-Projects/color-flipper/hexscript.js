adadfec8e8db767a16698db1770ce54578767623
Patiphan Orbesen <porbesen@cnm.edu>
2022-10-05 21:44:39 -0600

    setup event listener for the button

--- freeCodeCamp-JavaScript-Projects/color-flipper/hexscript.js
+++ freeCodeCamp-JavaScript-Projects/color-flipper/hexscript.js
@@ -1,3 +1,8 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+
+// add event listener for the button
+btn.addEventListener("click", function () {
+    let hexColor = "#";
+})
